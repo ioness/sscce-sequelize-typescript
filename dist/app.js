43414730e0637cb8aeb82f97248f1d0f699d9909
Your Name <you@example.com>
2017-12-30 23:47:07 +0100

    First commit, sscce 2.0

--- dist/app.js
+++ dist/app.js
@@ -2,8 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const app = express();
+app.get('/', (req, res) => {
+    res.json({ works: 1 });
+});
 app.listen(3000, () => {
-    console.log('Server started: sscce');
+    console.log('Server started: sscce ');
 });
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,12 @@ import * as express from 'express'
 
 const app: any = express()
 
+app.get('/', (req: any, res: any) => {
+  res.json({ works: 1 })
+})
+
 app.listen(3000, () => {
-  console.log('Server started: sscce')
+  console.log('Server started: sscce ')
 })
 
 export default app
